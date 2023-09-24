dbe172800f651202dc75ec997f9a9233575fccd1
Sabrina Bertol <bertol.sabrina@gmail.com>
2023-09-24 21:11:17 +0100

    minor fixes

--- src/components/QuoteGenerator.js
+++ src/components/QuoteGenerator.js
@@ -1,85 +1,89 @@
-import { useState, useEffect, useRef } from 'react';
-import { BiSolidChevronRightCircle, BiSolidQuoteAltLeft, BiStopCircle } from 'react-icons/bi';
-import { Tooltip } from 'react-tooltip';
-import styled from 'styled-components';
+import { useState, useEffect, useRef } from "react";
+import {
+  BiSolidChevronRightCircle,
+  BiSolidQuoteAltLeft,
+  BiStopCircle,
+} from "react-icons/bi";
+import { Tooltip } from "react-tooltip";
+import styled from "styled-components";
 
 const QuoteContainer = styled.div`
-    height: auto;
-    margin: 20px 0;
-    @media (max-width: 768px) {
-        max-width: 100%;
-    }
+  height: auto;
+  margin: 20px 0;
+  @media (max-width: 768px) {
+    max-width: 100%;
+  }
 `;
 
 const Quote = styled.div`
-    width: 800px;
-    display: flex;
-    height: auto;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    text-align: left;
-    @media (max-width: 768px) {
-      align-items: center;
-      text-align: center;
-      width: auto;
-      margin: 0 40px;
+  width: 800px;
+  display: flex;
+  height: auto;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  text-align: left;
+  @media (max-width: 768px) {
+    align-items: center;
+    text-align: center;
+    width: auto;
+    margin: 0 40px;
   }
 `;
 
 const Title = styled.h1`
-    color: ${props => props.theme.titleColor};
-    font-size: 4em;
-    padding: 0;
-    margin: 0;
-    `;
+  color: ${(props) => props.theme.titleColor};
+  font-size: 4em;
+  padding: 0;
+  margin: 0;
+`;
 
 const Text = styled.p`
-    color: ${props => props.theme.tagLineColor};
-    font-style: italic;
-    font-size: 1.5em;
-    font-weight: 200;
-    @media (max-width: 768px) {
-        font-size: 1.2em;
-      }
+  color: ${(props) => props.theme.tagLineColor};
+  font-style: italic;
+  font-size: 1.5em;
+  font-weight: 200;
+  @media (max-width: 768px) {
+    font-size: 1.2em;
+  }
 `;
 
 const Author = styled.p`
-    color: ${props => props.theme.titleColor};
-    text-align: right;
-    font-weight: 400;
-    margin-top: 0.5em;
-    font-size: 1.2em;
-    @media (max-width: 768px) {
-        font-size: 1em;
-      }
+  color: ${(props) => props.theme.titleColor};
+  text-align: right;
+  font-weight: 400;
+  margin-top: 0.5em;
+  font-size: 1.2em;
+  @media (max-width: 768px) {
+    font-size: 1em;
+  }
 `;
 
 const ButtonGroup = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    gap: 20px;
-    margin-top: 1em;
+  display: flex;
+  justify-content: flex-start;
+  gap: 20px;
+  margin-top: 1em;
 `;
 
 const ButtonNewQuote = styled.div`
-    color: ${props => props.theme.titleColor};
-    text-decoration: none;
-    font-size: 24px;
-    cursor: pointer;
-    &:hover {
+  color: ${(props) => props.theme.titleColor};
+  text-decoration: none;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
     opacity: 0.7;
-    }
+  }
 `;
 
 const ButtonAutoQuote = styled.div`
-    color: ${props => props.theme.titleColor};
-    text-decoration: none;
-    font-size: 24px;
-    cursor: pointer;
-    &:hover {
+  color: ${(props) => props.theme.titleColor};
+  text-decoration: none;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
     opacity: 0.7;
-    }
+  }
 `;
 
 const TooltipStyle = styled.div`
@@ -96,7 +100,7 @@ const QuoteGenerator = () => {
       "https://quotes15.p.rapidapi.com/quotes/random/",
       {
         headers: {
-          'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+          "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
           "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
         },
       }
@@ -118,42 +122,54 @@ const QuoteGenerator = () => {
       clearInterval(interval.current);
     }
     fetchNewQuote();
-
   }, [isAutoQuoteEnabled]);
 
   return (
-
     <QuoteContainer>
       <Quote>
         <Title>"</Title>
         <Text>{quoteData.text}"</Text>
         <Author>- {quoteData.author || "Unknown"}</Author>
-      <ButtonGroup>
-      
-      <ButtonNewQuote onClick={fetchNewQuote}>
-        <BiSolidQuoteAltLeft data-tooltip-id="newquote"
-        data-tooltip-content="New Quote"
-        data-tooltip-place="top" />
-        <TooltipStyle><Tooltip id="newquote" /></TooltipStyle>
-        </ButtonNewQuote>
-        
-      {isAutoQuoteEnabled ? 
-        <ButtonAutoQuote onClick={() => setIsAutoQuoteEnabled(!isAutoQuoteEnabled)}>
-          <BiStopCircle data-tooltip-id="stop"
-          data-tooltip-content="Stop Auto Quote"
-          data-tooltip-place="top" />
-          <TooltipStyle><Tooltip id="stop" /></TooltipStyle>
-        </ButtonAutoQuote>
-        : 
-        <ButtonAutoQuote onClick={() => setIsAutoQuoteEnabled(!isAutoQuoteEnabled)}>
-          <BiSolidChevronRightCircle data-tooltip-id="start"
-          data-tooltip-content="Start Auto Quote"
-          data-tooltip-place="top" />
-          <TooltipStyle><Tooltip id="start" /></TooltipStyle>
-        </ButtonAutoQuote>
-      }
+        <ButtonGroup>
+          <ButtonNewQuote onClick={fetchNewQuote}>
+            <BiSolidQuoteAltLeft
+              data-tooltip-id="newquote"
+              data-tooltip-content="New Quote"
+              data-tooltip-place="top"
+            />
+            <TooltipStyle>
+              <Tooltip id="newquote" />
+            </TooltipStyle>
+          </ButtonNewQuote>
 
-      </ButtonGroup>
+          {isAutoQuoteEnabled ? (
+            <ButtonAutoQuote
+              onClick={() => setIsAutoQuoteEnabled(!isAutoQuoteEnabled)}
+            >
+              <BiStopCircle
+                data-tooltip-id="stop"
+                data-tooltip-content="Stop Auto Quote"
+                data-tooltip-place="top"
+              />
+              <TooltipStyle>
+                <Tooltip id="stop" />
+              </TooltipStyle>
+            </ButtonAutoQuote>
+          ) : (
+            <ButtonAutoQuote
+              onClick={() => setIsAutoQuoteEnabled(!isAutoQuoteEnabled)}
+            >
+              <BiSolidChevronRightCircle
+                data-tooltip-id="start"
+                data-tooltip-content="Start Auto Quote"
+                data-tooltip-place="top"
+              />
+              <TooltipStyle>
+                <Tooltip id="start" />
+              </TooltipStyle>
+            </ButtonAutoQuote>
+          )}
+        </ButtonGroup>
       </Quote>
     </QuoteContainer>
   );
--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,10 +1,14 @@
-import styled from "styled-components";
+import { Tooltip } from "react-tooltip";
+import {
+  BiLogoLinkedinSquare,
+  BiLogoGithub,
+  BiCoffeeTogo,
+} from "react-icons/bi";
 import { CgSun } from "react-icons/cg";
 import { HiMoon } from "react-icons/hi";
-import { BiLogoLinkedinSquare, BiLogoGithub, BiCoffeeTogo } from 'react-icons/bi';
-import { Tooltip } from 'react-tooltip';
+import styled from "styled-components";
 
-import QuoteGenerator from './QuoteGenerator';
+import QuoteGenerator from "./QuoteGenerator";
 
 const Page = styled.div`
   position: fixed;
@@ -15,27 +19,27 @@ const Page = styled.div`
   align-items: center;
   height: 100vh;
   width: 100%;
-  background-color: ${props => props.theme.pageBackground};
-  transition: all .5s ease;
+  background-color: ${(props) => props.theme.pageBackground};
+  transition: all 0.5s ease;
 `;
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    width: 100%;
-    @media (max-width: 768px) {
-       margin: 0 20px;
-    }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+  @media (max-width: 768px) {
+    margin: 0 20px;
+  }
 `;
 
 export const Navbar = styled.div`
-    position: fixed;
-    top: 0;
-    z-index: 10;
-    margin: 10px 0;
+  position: fixed;
+  top: 0;
+  z-index: 10;
+  margin: 10px 0;
   height: 40px;
   width: 100%;
   display: flex;
@@ -43,7 +47,7 @@ export const Navbar = styled.div`
   justify-content: center;
   @media (max-width: 768px) {
     padding: 20px 20px;
-}
+  }
 `;
 
 export const NavbarContainer = styled.div`
@@ -56,19 +60,19 @@ export const NavbarContainer = styled.div`
 `;
 
 const Toggle = styled.button`
-    cursor: pointer;
-    height: 24px;
-    width: 24px;   
-    border-radius: 50%;
-    border: none;
-    background-color: transparent;
-    color: ${props => props.theme.titleColor};
-    &:focus {
-        outline: none;
-    }
-    &:hover {
-        opacity: 0.7;
- }
+  cursor: pointer;
+  height: 24px;
+  width: 24px;
+  border-radius: 50%;
+  border: none;
+  background-color: transparent;
+  color: ${(props) => props.theme.titleColor};
+  &:focus {
+    outline: none;
+  }
+  &:hover {
+    opacity: 0.7;
+  }
 `;
 
 const Footer = styled.div`
@@ -83,8 +87,7 @@ const Footer = styled.div`
   @media (max-width: 768px) {
     padding: 20px 20px;
     justify-content: center;
-
-}
+  }
 `;
 
 const FooterLinks = styled.div`
@@ -92,11 +95,11 @@ const FooterLinks = styled.div`
   align-items: flex-start;
   margin: 0 20px;
   & a {
-    color: ${props => props.theme.titleColor};
+    color: ${(props) => props.theme.titleColor};
     text-decoration: none;
     font-size: 24px;
     padding: 0px 10px;
-  } 
+  }
   & a:hover {
     opacity: 0.7;
   }
@@ -106,59 +109,79 @@ const TooltipStyle = styled.div`
   font-size: 1em;
 `;
 
-
 function Splash(props) {
-    function changeTheme() {
-        if (props.theme === "light") {
-            props.setTheme("dark");
-        } else {
-            props.setTheme("light");
-        }
-    };
+  function changeTheme() {
+    if (props.theme === "light") {
+      props.setTheme("dark");
+    } else {
+      props.setTheme("light");
+    }
+  }
 
-    const icon = props.theme === "light" ? <div><HiMoon size={24}  data-tooltip-id="darkmode"
-    data-tooltip-content="Dark Mode"
-    data-tooltip-place="right"/><TooltipStyle><Tooltip id="darkmode" /></TooltipStyle></div> : <div><CgSun size={24} data-tooltip-id="lightmode"
-    data-tooltip-content="Light Mode"
-    data-tooltip-place="right" /><TooltipStyle><Tooltip id="lightmode" /></TooltipStyle></div>;
-
-    return (
-      <Page>
-        <Navbar>
-          <NavbarContainer>
-          <Toggle onClick={changeTheme}>{icon}</Toggle>
-          </NavbarContainer>
-        </Navbar>
-        <Container>
-          <QuoteGenerator />
-        </Container>
-        <Footer>
-          <FooterLinks>
-            <a
-              href="https://github.com/sabrinabertol"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <BiLogoGithub />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/sabrinabertol/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <BiLogoLinkedinSquare />
-            </a>
-            <a
-              href="https://www.buymeacoffee.com/sabrinabertol"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <BiCoffeeTogo />
-            </a>
-          </FooterLinks>
-        </Footer>
-      </Page>
+  const icon =
+    props.theme === "light" ? (
+      <div>
+        <HiMoon
+          size={24}
+          data-tooltip-id="darkmode"
+          data-tooltip-content="Dark Mode"
+          data-tooltip-place="right"
+        />
+        <TooltipStyle>
+          <Tooltip id="darkmode" />
+        </TooltipStyle>
+      </div>
+    ) : (
+      <div>
+        <CgSun
+          size={24}
+          data-tooltip-id="lightmode"
+          data-tooltip-content="Light Mode"
+          data-tooltip-place="right"
+        />
+        <TooltipStyle>
+          <Tooltip id="lightmode" />
+        </TooltipStyle>
+      </div>
     );
-};
+
+  return (
+    <Page>
+      <Navbar>
+        <NavbarContainer>
+          <Toggle onClick={changeTheme}>{icon}</Toggle>
+        </NavbarContainer>
+      </Navbar>
+      <Container>
+        <QuoteGenerator />
+      </Container>
+      <Footer>
+        <FooterLinks>
+          <a
+            href="https://github.com/sabrinabertol"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BiLogoGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sabrinabertol/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BiLogoLinkedinSquare />
+          </a>
+          <a
+            href="https://www.buymeacoffee.com/sabrinabertol"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BiCoffeeTogo />
+          </a>
+        </FooterLinks>
+      </Footer>
+    </Page>
+  );
+}
 
 export default Splash;
