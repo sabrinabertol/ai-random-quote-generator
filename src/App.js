6d5e6f4bffdabb2ec333256b6036e13826c9ea68
Sabrina Bertol <bertol.sabrina@gmail.com>
2023-09-24 12:08:32 +0100

    recreated components using styled components, added light-dark mode function, added auto quote function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import QuoteGenerator from './QuoteGenerator';
-import Footer from './Footer';
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+
+import './App.css';
+import Splash from "./components/SplashScreen";
+
+
+const LightTheme = {
+  pageBackground: "#F4F4F4",
+  titleColor: "#F6BE00",
+  tagLineColor: "black"
+};
+
+const DarkTheme = {
+  pageBackground: "#121212",
+  titleColor: "#FFDE00",
+  tagLineColor: "lavender"
+}
+
+const themes = {
+  light: LightTheme,
+  dark: DarkTheme,
+}
 
 function App() {
+    const [theme, setTheme] = useState("light")
     return (
-        <div className="App">
-            <QuoteGenerator />
-            <Footer />
-        </div>
+      <ThemeProvider theme={themes[theme]}>
+        <Splash theme={theme} setTheme={setTheme} />
+      </ThemeProvider>
     );
-}
+  }
 
 export default App;
 
